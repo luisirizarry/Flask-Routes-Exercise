dcf78c24b32f4a7683fa739606cf4f7344a4cbe7
Luis Irizarry <lirizarry6178@gmail.com>
2024-09-04 19:19:13 -0400

    Added Comments

--- calc/app.py
+++ calc/app.py
@@ -5,24 +5,28 @@ app = Flask(__name__)
 
 @app.route('/add')
 def add():
+    """Add a and b parameters."""
     a = request.args["a"]
     b = request.args["b"]
     return f"{add(a, b)}"
 
 @app.route('/sub')
 def sub():
+    """Subtract a and b parameters."""
     a = request.args["a"]
     b = request.args["b"]
     return f"{sub(a, b)}"
 
 @app.route('/mult')
 def mult():
+    """Multiply a and b parameters."""
     a = request.args["a"]
     b = request.args["b"]
     return f"{mult(a, b)}"
 
 @app.route('/div')
 def div():
+    """Divide a and b parameters."""
     a = request.args["a"]
     b = request.args["b"]
     return f"{div(a, b)}"
--- greet/app.py
+++ greet/app.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 
 @app.route('/welcome')
 def welcome():
+    """Return welcome message."""
     return 'welcome'
 
 @app.route('/welcome/home')
 def welcome_home():
+    """Return welcome home message."""
     return 'welcome home'
 
 @app.route('/welcome/back')
 def welcome_back():
+    """Return welcome back message."""
     return 'welcome back'
